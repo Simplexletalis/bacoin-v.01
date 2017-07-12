710e8d0ccd6487acb59fede0ea7a4e77aa7eba3a
Kirk Steele <spunkybd@gmail.com>
2017-07-12 14:53:35 -0400

    copyright updates

--- build/ui_aboutdialog.h
+++ build/ui_aboutdialog.h
@@ -91,7 +91,7 @@ public:
         copyrightLabel->setObjectName(QString::fromUtf8("copyrightLabel"));
         copyrightLabel->setCursor(QCursor(Qt::IBeamCursor));
         copyrightLabel->setText(QString::fromUtf8("Copyright &copy; 2009-YYYY The Bitcoin developers\n"
-"Copyright &copy; 2011-YYYY The Litecoin developers"));
+"Copyright &copy; 2011-YYYY The Bacoin developers"));
         copyrightLabel->setTextFormat(Qt::RichText);
         copyrightLabel->setTextInteractionFlags(Qt::LinksAccessibleByMouse|Qt::TextSelectableByKeyboard|Qt::TextSelectableByMouse);
 
